8bb0f381b26edb5117dae6fb8f23828babe8239e
Alan Christie <alan.christie@matildapeak.com>
2022-04-18 21:17:56 +0100

    build: Siwitch to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,54 +1,46 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # Setup module for the Data Manager Job Decoder module
 #
-# January 2022
+# April 2022
 
-from setuptools import setup
 import os
+import setuptools
+
 
 # Pull in the essential run-time requirements
-with open('requirements.txt') as file:
-    requirements = file.read().splitlines()
-
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read().splitlines()
 
 # Use the README.rst as the long description.
-def get_long_description():
-    return open('README.rst').read()
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-
-setup(
-
-    name='im-data-manager-job-decoder',
-    version=os.environ.get('GITHUB_REF_SLUG', '1.0.0'),
-    author='Alan Christie',
-    author_email='achristie@informaticsmatters.com',
-    url='https://github.com/informaticsmatters/data-manager-job-decoder',
-    license='MIT',
-    description='Job decoding logic',
-    long_description=get_long_description(),
-    keywords='jinja2 decoder',
-    platforms=['any'],
-
+setuptools.setup(
+    name="im-data-manager-job-decoder",
+    version=os.environ.get("GITHUB_REF_SLUG", "1.0.0"),
+    author="Alan Christie",
+    author_email="achristie@informaticsmatters.com",
+    url="https://github.com/informaticsmatters/data-manager-job-decoder",
+    license="MIT",
+    description="Job decoding logic",
+    long_description=long_description,
+    keywords="jinja2 decoder",
+    platforms=["any"],
     # Our modules to package
-    packages=['decoder'],
+    packages=["decoder"],
     include_package_data=True,
-
     # Project classification:
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Other Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Operating System :: POSIX :: Linux',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: POSIX :: Linux",
     ],
-
     install_requires=requirements,
-
     zip_safe=False,
-
 )
